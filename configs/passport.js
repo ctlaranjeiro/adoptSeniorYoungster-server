d942965597f74ba0733a9a9af7028839a2cee7be
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-07-01 23:26:45 +0100

    Adds auth-routes complete and passport.js with 2 LocalStrategies

---
+++ configs/passport.js
@@ -0,0 +1,71 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt        = require('bcrypt');
+const passport      = require('passport');
+const User          = require('../models/user');
+const Volunteer     = require('../models/volunteer');
+
+passport.serializeUser((loggedInUser, cb) => {
+  cb(null, loggedInUser._id);
+});
+
+passport.deserializeUser((userIdFromSession, cb) => {
+  User.findById(userIdFromSession)
+    .then(user => {
+        console.log('deserialize user:', user);
+        if(user){
+            User.findById(userIdFromSession, (err, userDocument) => {
+                console.log('found user in User Collection');
+                if (err) {
+                  cb(err);
+                  return;
+                }
+                cb(null, userDocument);
+              });
+        }else{
+            Volunteer.findById(userIdFromSession, (err, volunteerDocument) => {
+                console.log('found volunteer in Volunteer Collection');
+                if (err) {
+                  cb(err);
+                  return;
+                }
+                cb(null, volunteerDocument);
+              });
+        }
+    })
+});
+
+passport.use('user', new LocalStrategy((email, password, next) => {
+  User.findOne({ email }, (err, foundUser) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    if (!foundUser) {
+      next(null, false, { message: 'Incorrect email.' });
+      return;
+    }
+    if (!bcrypt.compareSync(password, foundUser.password)) {
+      next(null, false, { message: 'Incorrect password.' });
+      return;
+    }
+    next(null, foundUser);
+  });
+}));
+
+passport.use('volunteer', new LocalStrategy((email, password, next) => {
+    Volunteer.findOne({ email }, (err, foundVolunteer) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      if (!foundVolunteer) {
+        next(null, false, { message: 'Incorrect email.' });
+        return;
+      }
+      if (!bcrypt.compareSync(password, foundVolunteer.password)) {
+        next(null, false, { message: 'Incorrect password.' });
+        return;
+      }
+      next(null, foundVolunteer);
+    });
+  }));
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -9,7 +9,7 @@ const Volunteer = require('../models/volunteer');
 const Institution = require('../models/institution');
 
 authRoutes.post('/signup/:accountType', (req, res, next) => {
-    const params = req.params.accountType;
+    const accountType = req.params.accountType;
 
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
@@ -19,7 +19,12 @@ authRoutes.post('/signup/:accountType', (req, res, next) => {
         return string.toLowerCase();
     }
 
-    if(params === 'user'){
+    //age based on birthDate
+    function getYears(x) {
+        return Math.floor(x / 1000 / 60 / 60 / 24 / 365);
+    }
+
+    if(accountType === 'user'){
         const {
             email,
             password,
@@ -83,11 +88,11 @@ authRoutes.post('/signup/:accountType', (req, res, next) => {
             res.status(400).json({ message: 'Provide phone number' });
             return;
         }
-        if (!morning || !afternoon || !evening || !night || !overNight || !fullDay) {
+        if (!morning && !afternoon && !evening && !night && !overNight && !fullDay) {
             res.status(400).json({ message: 'Provide schedule preferences: morning, afternoon, evening, night, over night or full day' });
             return;
         }
-        if (!healthCare || !houseCare || !displacements || !grocery || !pupil) {
+        if (!healthCare && !houseCare && !displacements && !grocery && !pupil) {
             res.status(400).json({ message: 'Provide needs: health care, house care, displacements, grocery or pupil' });
             return;
         }
@@ -174,9 +179,6 @@ authRoutes.post('/signup/:accountType', (req, res, next) => {
 
 
         // calculate age based on birthDate
-        function getYears(x) {
-            return Math.floor(x / 1000 / 60 / 60 / 24 / 365);
-        }
         const n = Date.now();
         const d = new Date(birthDate);
         const ageFromDate = getYears(n - d);
@@ -226,64 +228,267 @@ authRoutes.post('/signup/:accountType', (req, res, next) => {
                     return;
                 }
 
-                req.session.currentUser = newUser;
-                res.status(200).json(newUser);
+                // req.session.currentUser = newUser;
+                // res.status(200).json(newUser);
                 
-                // req.login(newUser, (err) => {
-                //     if (err) {
-                //         res.status(500).json({ message: 'Login after signup went bad.' });
-                //         console.log('err:', err);
-                //         return;
-                //     }
-                //     // Send the user's information to the frontend
-                //     res.status(200).json(newUser);
-                // });
+                req.login(newUser, (err) => {
+                    if (err) {
+                        res.status(500).json({ message: 'Login after signup went bad.' });
+                        console.log('err:', err);
+                        return;
+                    }
+                    // Send the user's information to the frontend
+                    res.status(200).json(newUser);
+                });
             });
         });
-    } /* END OF IF ACCOUNT TYPE 'USER' */
+    } /* ----------------------   END OF IF ACCOUNT TYPE 'USER' */
+
+    if(accountType === 'volunteer'){
+        const {
+            email,
+            password,
+            firstName,
+            lastName,
+            gender,
+            birthDate,
+            address,
+            volPhoneNumber,
+            occupation,
+            //available periods
+            morning,
+            afternoon,
+            evening,
+            night,
+            overNight,
+            fullDay,
+            //skills
+            healthCare,
+            houseCare,
+            displacements,
+            grocery,
+            pupil,
+            aboutMe
+        } = req.body;
+
+        // CHECK IF FIELDS EXIST
+        if (!email) {
+            res.status(400).json({ message: 'Provide email' });
+            return;
+        }
+        if (!password) {
+            res.status(400).json({ message: 'Provide password' });
+            return;
+        }
+        if (!firstName) {
+            res.status(400).json({ message: 'Provide first name' });
+            return;
+        }
+        if (!lastName) {
+            res.status(400).json({ message: 'Provide last name' });
+            return;
+        }
+        if (!gender) {
+            res.status(400).json({ message: 'Provide gender' });
+            return;
+        }
+        if (!birthDate) {
+            res.status(400).json({ message: 'Provide birth date' });
+            return;
+        }
+        if (!address) {
+            res.status(400).json({ message: 'Provide address' });
+            return;
+        }
+        if (!volPhoneNumber) {
+            res.status(400).json({ message: 'Provide phone number' });
+            return;
+        }
+        if (!morning && !afternoon && !evening && !night && !overNight && !fullDay) {
+            res.status(400).json({ message: 'Provide availability: morning, afternoon, evening, night, over night or full day' });
+            return;
+        }
+        if (!healthCare && !houseCare && !displacements && !grocery && !pupil) {
+            res.status(400).json({ message: 'Provide skills: health care, house care, displacements, grocery or pupil' });
+            return;
+        }
+
+
+        // CHECK PASSWORD LENGTH
+        if(password.length < 6){
+            res.status(400).json({ message: 'Please make your password at least 6 characters long for security purposes.' });
+            return;
+        }
+
+        // CHECK PHONE NUMBER LENGTH
+        if(volPhoneNumber.length !== 9){
+            res.status(400).json({ message: 'Please insert a valid phone number with 9 digits.' });
+            return;
+        }
+
+        // ---- CHECKBOX VALUES 
+        const availablePeriodsArr = [];
+
+        if (morning) {
+        availablePeriodsArr.push(morning);
+        }
+        if (afternoon) {
+        availablePeriodsArr.push(afternoon);
+        }
+        if (evening) {
+        availablePeriodsArr.push(evening);
+        }
+        if (night) {
+        availablePeriodsArr.push(night);
+        }
+        if (overNight) {
+        availablePeriodsArr.push(overNight);
+        }
+        if (fullDay) {
+        availablePeriodsArr.push(fullDay);
+        }
+
+        const skillsArr = [];
+
+        if (healthCare) {
+        skillsArr.push(healthCare);
+        }
+        if (houseCare) {
+        skillsArr.push(houseCare);
+        }
+        if (displacements) {
+        skillsArr.push(displacements);
+        }
+        if (grocery) {
+        skillsArr.push(grocery);
+        }
+        if (pupil) {
+        skillsArr.push(pupil);
+        }
+
+        // calculate age based on birthDate
+        const n = Date.now();
+        const d = new Date(birthDate);
+        const ageFromDate = getYears(n - d);
+
+        // ENCRYPT PASSWORD        
+        const salt     = bcrypt.genSaltSync(10);
+        const hashPass = bcrypt.hashSync(password, salt);
+
+
+        // NEW VOLUNTEER
+        const newVolunteer = new Volunteer({
+            email: lowerCaseLetters(email),
+            password: hashPass,
+            firstName: capitalizeFirstLetter(firstName),
+            lastName: capitalizeFirstLetter(lastName),
+            gender,
+            birthDate,
+            age: ageFromDate,
+            address: capitalizeFirstLetter(address),
+            volPhoneNumber,
+            occupation: capitalizeFirstLetter(occupation),
+            availablePeriods: availablePeriodsArr,
+            skills: skillsArr,
+            aboutMe,
+          });
+
+
+          Volunteer.findOne({ email }, (err, foundVolunteer) => {
+            if(err){
+                res.status(500).json({message: "Volunteer email check went bad."});
+                return;
+            }
+            
+            if (foundVolunteer) {
+                res.status(400).json({ message: 'Email already registered. Try to login' });
+                return;
+            }
+            
+            newVolunteer.save(err => {
+                if (err) {
+                    res.status(400).json({ message: 'Saving volunteer to database went wrong.' });
+                    console.log('err:', err);
+                    return;
+                }
+                
+                req.login(newVolunteer, (err) => {
+                    if (err) {
+                        res.status(500).json({ message: 'Login after signup went bad.' });
+                        console.log('err:', err);
+                        return;
+                    }
+                    // Send the volunteers's information to the frontend
+                    res.status(200).json(newVolunteer);
+                });
+            });
+        });
+       
+    } /* ----------------------   END OF IF ACCOUNT TYPE 'VOLUNTEER' */
+
 });
 
-authRoutes.post('/login', (req, res, next) => {
-  passport.authenticate('local', (err, theUser, failureDetails) => {
-      if (err) {
-          res.status(500).json({ message: 'Something went wrong authenticating user' });
-          return;
-      }
-      
-      if (!theUser) {
-          res.status(401).json(failureDetails);
-          return;
-      }
-      
-      // save user in session
-      req.login(theUser, (err) => {
-          if (err) {
-              res.status(500).json({ message: 'Session save went bad.' });
-              return;
-          }
-          res.status(200).json(theUser);
-      });
-  })(req, res, next);
+authRoutes.post('/login/:accountType', (req, res, next) => {
+    const accountType = req.params.accountType;
+
+    if(accountType === 'user'){
+        passport.authenticate('user', (err, theUser, failureDetails) => {
+            if (err) {
+                res.status(500).json({ message: 'Something went wrong authenticating user' });
+                return;
+            }
+            
+            if (!theUser) {
+                res.status(401).json(failureDetails);
+                return;
+            }
+            
+            // save user in session
+            req.login(theUser, (err) => {
+                if (err) {
+                    res.status(500).json({ message: 'Session save went bad.' });
+                    return;
+                }
+                res.status(200).json(theUser);
+            });
+        })(req, res, next);
+    }
+
+    if(accountType === 'volunteer'){
+        passport.authenticate('volunteer', (err, theVolunteer, failureDetails) => {
+            if (err) {
+                res.status(500).json({ message: 'Something went wrong authenticating user' });
+                return;
+            }
+            
+            if (!theVolunteer) {
+                res.status(401).json(failureDetails);
+                return;
+            }
+            
+            // save user in session
+            req.login(theVolunteer, (err) => {
+                if (err) {
+                    res.status(500).json({ message: 'Session save went bad.' });
+                    return;
+                }
+                res.status(200).json(theVolunteer);
+            });
+        })(req, res, next);
+    }
+    
 });
 
 authRoutes.post('/logout', (req, res, next) => {
     //req.logout() is defined by passport
-    //req.logout();
-    req.session.destroy(() => {
-        res.status(200).json({ message: 'Log out success!' });
-    });
-    
+    req.logout();
+    res.status(200).json({ message: 'Log out successful.' });
 });
 
 authRoutes.get('/loggedin', (req, res, next) => {
-  // req.isAuthenticated() is defined by passport
-//   if (req.isAuthenticated()) {
-//       res.status(200).json(req.user);
-//       return;
-//   }
-
-    if (req.session.currentUser) {
-        res.status(200).json(req.session.currentUser);
+    // req.isAuthenticated() is defined by passport
+    if (req.isAuthenticated()) {
+        res.status(200).json(req.user);
         return;
     }
 
