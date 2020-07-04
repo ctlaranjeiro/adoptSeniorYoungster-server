8792c55a7b904b190dc32026b8ac07ef5aae5f96
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-07-04 16:40:22 +0100

    Adds actions to editUser-routes and adds populate to auth-routes

--- routes/editUser-routes.js
+++ routes/editUser-routes.js
@@ -1,5 +1,6 @@
-const express    = require('express');
-const editUserRoutes = express.Router();
+const express           = require('express');
+const editUserRoutes    = express.Router();
+const bcrypt            = require('bcrypt');
 
 // require user, volunteer and institution models
 const User = require('../models/user');
@@ -13,6 +14,14 @@ editUserRoutes.put('/user/:id/edit/:action', (req, res, next) => {
     // console.log('id', currentUser._id);
     const action = req.params.action;
 
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    
+    function lowerCaseLetters(string) {
+        return string.toLowerCase();
+    }
+
     const {
         //personal details
         firstName,
@@ -21,6 +30,19 @@ editUserRoutes.put('/user/:id/edit/:action', (req, res, next) => {
         address,
         phoneNumber,
         userNotes,
+        //emergency contact
+        emergFirstName,
+        emergLastName,
+        emergPhoneNumber,
+        emergEmail,
+        emergAddress,
+        password,
+        assignedVolunteer,
+        volunteer
+    } = req.body;
+
+    //interchangeable values
+    let {
         //schedule preferences
         morning,
         afternoon,
@@ -33,75 +55,36 @@ editUserRoutes.put('/user/:id/edit/:action', (req, res, next) => {
         houseCare,
         displacements,
         grocery,
-        pupil,
-        //emergency contact
-        emergFirstName,
-        emergLastName,
-        emergPhoneNumber,
-        emergEmail,
-        emergAddress,
-        password,
-        assignedVolunteer,
-        volunteer
+        pupil
     } = req.body;
 
+
     // ---- CHECKBOX VALUES 
-    const schedulePreferenceArr = [];
+    if(fullDay){
+        morning = true;
+        afternoon = true;
+        evening = true;
+        night = true;
+        overNight = true;
+        fullDay = true;
+    }
 
-    if (morning) {
-        schedulePreferenceArr.push(morning);
-        if (fullDay) {
-            schedulePreferenceArr.pop();
-            schedulePreferenceArr.push(fullDay);
-        }
+    if(!healthCare){
+        healthCare = false;
     }
-    if (afternoon) {
-        schedulePreferenceArr.push(afternoon);
-        if (fullDay) {
-            schedulePreferenceArr.pop();
-            schedulePreferenceArr.push(fullDay);
-        }
+    if(!houseCare){
+        houseCare = false;
     }
-    if (evening) {
-        schedulePreferenceArr.push(evening);
-        if (fullDay) {
-            schedulePreferenceArr.pop();
-            schedulePreferenceArr.push(fullDay);
-        }
+    if(!displacements){
+        displacements = false;
     }
-    if (night) {
-        schedulePreferenceArr.push(night);
-        if (fullDay) {
-            schedulePreferenceArr.pop();
-            schedulePreferenceArr.push(fullDay);
-        }
+    if(!grocery){
+        grocery = false;
     }
-    if (overNight) {
-        schedulePreferenceArr.push(overNight);
-        if (fullDay) {
-            schedulePreferenceArr.pop();
-            schedulePreferenceArr.push(fullDay);
-        }
+    if(!pupil){
+        pupil = false;
     }
-    
 
-    const specificNeedsArr = [];
-
-    if (healthCare) {
-        specificNeedsArr.push(healthCare);
-    }
-    if (houseCare) {
-        specificNeedsArr.push(houseCare);
-    }
-    if (displacements) {
-        specificNeedsArr.push(displacements);
-    }
-    if (grocery) {
-        specificNeedsArr.push(grocery);
-    }
-    if (pupil) {
-        specificNeedsArr.push(pupil);
-    }
 
     //PERSONAL DETAILS
     if(action === 'personalDetails'){
@@ -156,7 +139,14 @@ editUserRoutes.put('/user/:id/edit/:action', (req, res, next) => {
     //SCHEDULE
     if(action === 'schedulePreferences'){
         User.updateOne({ _id: currentId }, { $set: { 
-            schedulePreference: schedulePreferenceArr
+            schedulePreference: {
+                morning: morning,
+                afternoon: afternoon,
+                evening: evening,
+                night: night,
+                overNight: overNight,
+                fullDay: fullDay
+            }
         }})
             .then(updatedUser => {
                 console.log('User schedule preferences updated!', updatedUser);
@@ -176,6 +166,89 @@ editUserRoutes.put('/user/:id/edit/:action', (req, res, next) => {
     }
 
 
+    //NEEDS
+    if(action === 'specificNeeds'){
+        User.updateOne({ _id: currentId }, { $set: { 
+            specificNeeds: {
+                healthCare: healthCare,
+                houseCare: houseCare,
+                displacements: displacements,
+                grocery: grocery,
+                pupil: pupil
+            }
+        }})
+            .then(updatedUser => {
+                console.log('User specific needs updated!', updatedUser);
+                
+                User.findById(currentId)
+                    .then(userFromDB => {
+                        res.status(200).json(userFromDB);
+                    })
+                    .catch(err => {
+                        console.log('Error:', err);
+                        res.status(400).json({ message: 'Error while finding user from DB'});
+                    });
+            })
+            .catch(err => {
+                console.log('Error while updating user specific needs in DB:', err);
+            });
+    }
+
+
+    //EMERGENCY CONTACT
+    if(action === 'emergContact'){
+        User.updateOne({ _id: currentId }, { $set: { 
+            emergencyContact: {
+                firstName: emergFirstName,
+                lastName: emergLastName,
+                phoneNumber: emergPhoneNumber,
+                email: emergEmail,
+                address: emergAddress
+            }
+        }})
+            .then(updatedUser => {
+                console.log('User emergency contact updated!', updatedUser);
+                
+                User.findById(currentId)
+                    .then(userFromDB => {
+                        res.status(200).json(userFromDB);
+                    })
+                    .catch(err => {
+                        console.log('Error:', err);
+                        res.status(400).json({ message: 'Error while finding user from DB'});
+                    });
+            })
+            .catch(err => {
+                console.log('Error while updating user emergency contact in DB:', err);
+            });
+    }
+
+
+    //PASSWORD
+    if(action === 'password'){
+        // ENCRYPT PASSWORD 
+        const salt = bcrypt.genSaltSync(10);
+        const hashPass = bcrypt.hashSync(password, salt);
+
+        User.updateOne({ _id: currentId }, { $set: { 
+            password: hashPass
+        }})
+            .then(updatedUser => {
+              console.log('User password updated!', updatedUser);
+
+              User.findById(currentId)
+                    .then(userFromDB => {
+                        res.status(200).json(userFromDB);
+                    })
+                    .catch(err => {
+                        console.log('Error:', err);
+                        res.status(400).json({ message: 'Error while finding user from DB'});
+                    });
+            })
+            .catch(err => {
+              console.log('Error while updating user password in DB:', err);
+            });
+    }
 
 
 });
