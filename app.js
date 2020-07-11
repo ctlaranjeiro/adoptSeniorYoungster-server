8fd99b6124e1809a4e07520af122844beea6015d
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-07-11 11:07:34 +0100

    Heroku deploy app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ require('./configs/passport');
 
 
 mongoose
-  .connect('mongodb://localhost/adoptSeniorYoungster-server', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
