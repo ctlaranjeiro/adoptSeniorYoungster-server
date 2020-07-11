af9a6195f55e23f5cd8cb8e7ce2d7fe1f76bdd68
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-07-11 11:16:44 +0100

    Heroku deploy mongoose.connection

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ require('./configs/passport');
 
 
 mongoose
-  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
