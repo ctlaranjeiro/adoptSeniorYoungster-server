e538b79bf5359c5216d7c25949c64e68d806fd25
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-07-13 21:05:11 +0100

    CheckIfRegistered trying to fix4

--- routes/checkIfRegistered-routes.js
+++ routes/checkIfRegistered-routes.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 const Volunteer = require('../models/volunteer');
 
 registeredRoutes.get('/checkIfRegistered', (req, res, next) => {
-    const { email } = req.body.data;
+    const { email } = req.body;
 
     User.findOne({ email }, (err, foundUser) => {
         if(err){
