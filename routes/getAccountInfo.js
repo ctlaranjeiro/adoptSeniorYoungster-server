474fee1cf47834cb1e526f74213682dfdf0ae0fe
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-07-23 17:47:06 +0100

    Populates and sends JSON with Reports and Reviews

--- routes/getAccountInfo.js
+++ routes/getAccountInfo.js
@@ -2,6 +2,8 @@ const express    = require('express');
 const getAccountInfoRoutes = express.Router();
 const User = require('../models/user');
 const Volunteer = require('../models/volunteer');
+const Report = require('../models/report');
+const Review = require('../models/review');
 
 getAccountInfoRoutes.get('/:account/:id', (req, res, next) => {
     const {account, id} = req.params;
@@ -10,10 +12,17 @@ getAccountInfoRoutes.get('/:account/:id', (req, res, next) => {
         User.findById(id)
             .populate('assignedVolunteers')
             .then(userFromDB => {
-                res.status(200).json(userFromDB);
+                Report.find({'subject': userFromDB._id})
+                    .populate('author')
+                    .then(reportFromDB => {
+                        res.status(200).json({ account: userFromDB , reports: reportFromDB});
+                    })
+                    .catch(err => {
+                        res.status(400).json({ message: 'Error while fetching user and report from DB', err });
+                    });
             })
             .catch(err => {
-                res.status(400).json({ message: 'Error while fetching user from DB' });
+                res.status(400).json({ message: 'Error while fetching user from DB', err });
             });
     }
     
@@ -21,10 +30,17 @@ getAccountInfoRoutes.get('/:account/:id', (req, res, next) => {
         Volunteer.findById(id)
             .populate('assignedUsers')
             .then(volunteerFromDB => {
-                res.status(200).json(volunteerFromDB);
+                Review.find({ 'subject': volunteerFromDB._id })
+                    .populate('author')
+                    .then(reviewFromDB => {
+                        res.status(200).json({ account: volunteerFromDB , reviews: reviewFromDB});
+                    })
+                    .catch(err => {
+                        res.status(400).json({ message: 'Error while fetching volunteer and reviews from DB', err });
+                    });
             })
             .catch(err => {
-                res.status(400).json({ message: 'Error while fetching volunteer from DB' });
+                res.status(400).json({ message: 'Error while fetching volunteer from DB', err });
             });
     }
 });
