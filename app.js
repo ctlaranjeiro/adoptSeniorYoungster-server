255de5bd58e53a8d17d191919c9f0ec90d07835e
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-09-15 14:29:28 +0100

    removes trust proxy

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ app.use(require('node-sass-middleware')({
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.set('trust proxy', 1);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
