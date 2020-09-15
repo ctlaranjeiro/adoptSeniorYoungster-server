0a41789d74a29b2babfe49c0b40718c9d440892b
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-09-15 14:41:47 +0100

    sets session to original state

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ app.use(require('node-sass-middleware')({
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.set('trust proxy', 1);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
@@ -61,7 +60,7 @@ app.use(session ({
   resave: true,
   saveUnitialized: true,
   rolling: true,
-  cookie: { expires: 600000, secure: true }
+  cookie: { expires: 600000 }
 }));
 
 // PASSPORT SETTINGS:
