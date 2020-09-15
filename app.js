4d4f25063a4f5c4d03a10b3744d41881fbeb7058
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-09-15 14:36:40 +0100

    changes session

--- app.js
+++ app.js
@@ -51,16 +51,17 @@ app.use(require('node-sass-middleware')({
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.set('trust proxy', 1);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // SESSION SETTINGS:
 app.use(session ({
   secret: "adoptSeniorYoungster",
-  resave: true,
+  resave: false,
   saveUnitialized: true,
   rolling: true,
-  cookie: { expires: 600000 }
+  cookie: { expires: 600000, secure: true }
 }));
 
 // PASSPORT SETTINGS:
